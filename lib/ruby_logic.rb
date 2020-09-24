81ebfef79773326984582305a47592f22142051b
Alma <a.q.frankenstein@gmail.com>
2020-09-24 11:12:36 -0700

    pass test to get value from key in @coin
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -2,8 +2,6 @@ class CoinCounter
   attr_accessor(:cents)
   attr_reader(:coin)
 
-
-
   def initialize(cents)
     @cents = cents
     @coin = {0.25 => "quarter", 0.10 => "dime", 0.05 => "nickel", 0.01 => "penny"}
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -7,4 +7,14 @@ describe ('CoinCounter#initialize') do
     test = CoinCounter.new(0.35)
     expect(test.cents).to(eq(0.35))
   end
+
+  it('initializes coin') do
+    test = CoinCounter.new(0.25)
+    expect(test.coin).to(eq({0.25 => "quarter", 0.10 => "dime", 0.05 => "nickel", 0.01 => "penny"}))
+  end
+
+  it('get value of a key in @coin') do
+    test = CoinCounter.new(0.25)
+    expect(test.coin.fetch(0.10)).to(eq("dime"))
+  end
 end
